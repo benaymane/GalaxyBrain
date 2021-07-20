38090e2f0c2ad2bf5ae1e96801087d5fe516aa11
benaymane <ben.aymane@gmail.com>
2021-07-19 22:03:20 -0700

    Reconfigure the SceneLoader into a ButtonController script - Added functionality to How_To_Play button

---
+++ Assets/Script/ButtonController.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonController : MonoBehaviour {
+    public void loadNextScene() {
+        int sceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(sceneIndex + 1);
+    }
+
+    public void changePanelVisibility(GameObject panel) {
+        bool activeStatus = panel.activeSelf;
+        panel.SetActive(!activeStatus);
+    }
+}
